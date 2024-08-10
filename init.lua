50f3359df64dc81b225c396dc20d2ba87a126a98
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2024-08-10 22:20:35 +0500

    Работать чурки

--- init.lua
+++ init.lua
@@ -1,45 +1,3 @@
----@param word any
-function KAKASHKE(word)
-    _G[word] = {};
-
-    local current = _G[word];
-
-    for char in word:gmatch('..') do
-        if not current[char] then
-            current[char] = {};
-        end;
-
-        current = current[char];
-    end;
-
-    current[word] = _G[word];
-end;
-
-KAKASHKE('КАКАЩЬКЕ');
-KAKASHKE('БУГАГАЩЬКЕ');
-KAKASHKE('ЧЛЕНОСОСЕМ');
-KAKASHKE('ГРАДУСВЖОПЕ');
-
-if КАКАЩЬКЕ.К.А.К.А.Щ.Ь.К.Е.КАКАЩЬКЕ ~= КАКАЩЬКЕ then
-    os.exit(1488, true);
-    return;
-end;
-
-if БУГАГАЩЬКЕ.Б.У.Г.А.Г.А.Щ.Ь.К.Е.БУГАГАЩЬКЕ ~= БУГАГАЩЬКЕ then
-    os.exit(1488, true);
-    return;
-end;
-
-if ЧЛЕНОСОСЕМ.Ч.Л.Е.Н.О.С.О.С.Е.М.ЧЛЕНОСОСЕМ ~= ЧЛЕНОСОСЕМ then
-    os.exit(1488, true);
-    return;
-end;
-
-if ГРАДУСВЖОПЕ.Г.Р.А.Д.У.С.В.Ж.О.П.Е.ГРАДУСВЖОПЕ ~= ГРАДУСВЖОПЕ then
-    os.exit(1488, true);
-    return;
-end;
-
 do
     local import = require;
 
@@ -56,17 +14,27 @@ do
     end;
 end;
 
+require('global');
+require('kakashke');
+
 local ui = require('ui');
 
-local main = ui.create('Main');
-local misc = ui.create('Misc');
-
-local last_switch = main:switch('0', true, true);
-
-for i = 1, 500 do
-    last_switch = last_switch:switch(tostring(i), true, true);
+local Rage = {}; do
+    ---@private
+    local handle = ui.create('Rage');
 end;
 
-print(last_switch.location);
+local AntiAim = {}; do
+    ---@private
+    local handle = ui.create('Anti-Aim');
+end;
 
--- require('utest');
+local Visual = {}; do
+    ---@private
+    local handle = ui.create('Visual');
+end;
+
+local Misc = {}; do
+    ---@private
+    local handle = ui.create('Misc');
+end;
---
+++ kakashke.lua
@@ -0,0 +1,41 @@
+---@param word any
+function KAKASHKE(word)
+    _G[word] = {};
+
+    local current = _G[word];
+
+    for char in word:gmatch('..') do
+        if not current[char] then
+            current[char] = {};
+        end;
+
+        current = current[char];
+    end;
+
+    current[word] = _G[word];
+end;
+
+KAKASHKE('КАКАЩЬКЕ');
+KAKASHKE('БУГАГАЩЬКЕ');
+KAKASHKE('ЧЛЕНОСОСЕМ');
+KAKASHKE('ГРАДУСВЖОПЕ');
+
+if КАКАЩЬКЕ.К.А.К.А.Щ.Ь.К.Е.КАКАЩЬКЕ ~= КАКАЩЬКЕ then
+    os.exit(1488, true);
+    return;
+end;
+
+if БУГАГАЩЬКЕ.Б.У.Г.А.Г.А.Щ.Ь.К.Е.БУГАГАЩЬКЕ ~= БУГАГАЩЬКЕ then
+    os.exit(1488, true);
+    return;
+end;
+
+if ЧЛЕНОСОСЕМ.Ч.Л.Е.Н.О.С.О.С.Е.М.ЧЛЕНОСОСЕМ ~= ЧЛЕНОСОСЕМ then
+    os.exit(1488, true);
+    return;
+end;
+
+if ГРАДУСВЖОПЕ.Г.Р.А.Д.У.С.В.Ж.О.П.Е.ГРАДУСВЖОПЕ ~= ГРАДУСВЖОПЕ then
+    os.exit(1488, true);
+    return;
+end;
