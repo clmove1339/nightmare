45003803a34529a30fc553594a2fcb2bc7a31921
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2024-08-20 21:40:33 +0500

    SRITE SAMI S VOLOSAMI

--- init.lua
+++ init.lua
@@ -17,3 +17,23 @@ end;
 require('global');
 
 local ui = require('ui');
+local subscription_level = 1;
+
+local antiaim = {}; do
+    ---@private
+    local handle = ui.create('Anti-Aim');
+
+    antiaim.builder = {}; do
+        local states = { 'Default', 'Run', 'Walk', 'Crouch', 'In Air' };
+        local information = {};
+        local group = handle:switch('Builder', false, true);
+
+        for _, state in ipairs(states) do
+            information[state] = {};
+        end;
+
+        group:combo('State', states, 0);
+
+        --
+    end;
+end;
