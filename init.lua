ddd117a68303673c9ab3ebf22ad0eb8289439679
X0m9K <101306717+seize1337@users.noreply.github.com>
2024-08-20 19:43:29 +0400

    убраны какащкэ

--- init.lua
+++ init.lua
@@ -15,26 +15,5 @@ do
 end;
 
 require('global');
-require('kakashke');
 
 local ui = require('ui');
-
-local Rage = {}; do
-    ---@private
-    local handle = ui.create('Rage');
-end;
-
-local AntiAim = {}; do
-    ---@private
-    local handle = ui.create('Anti-Aim');
-end;
-
-local Visual = {}; do
-    ---@private
-    local handle = ui.create('Visual');
-end;
-
-local Misc = {}; do
-    ---@private
-    local handle = ui.create('Misc');
-end;
