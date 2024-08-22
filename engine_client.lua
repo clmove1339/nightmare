ca37c29502a996461dd1b78424621ddf5f49cd0a
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2024-08-22 16:43:54 +0500

    Added annotations to engine client.lua

--- engine_client.lua
+++ engine_client.lua
@@ -1,5 +1,33 @@
 local memory = require 'memory';
 
+---@class INetChannelInfo
+---@field GetName fun(self: INetChannelInfo): string;
+---@field GetAddress fun(self: INetChannelInfo): string;
+---@field GetTime fun(self: INetChannelInfo): number;
+---@field GetTimeConnected fun(self: INetChannelInfo): number;
+---@field GetBufferSize fun(self: INetChannelInfo): number;
+---@field GetDataRate fun(self: INetChannelInfo): number;
+---@field IsLoopback fun(self: INetChannelInfo): boolean;
+---@field IsTimingOut fun(self: INetChannelInfo): boolean;
+---@field IsPlayback fun(self: INetChannelInfo): boolean;
+---@field GetLatency fun(self: INetChannelInfo, flow: number): number;
+---@field GetAvgLatency fun(self: INetChannelInfo, flow: number): number;
+---@field GetAvgLoss fun(self: INetChannelInfo, flow: number): number;
+---@field GetAvgChoke fun(self: INetChannelInfo, flow: number): number;
+---@field GetAvgData fun(self: INetChannelInfo, flow: number): number;
+---@field GetAvgPackets fun(self: INetChannelInfo, flow: number): number;
+---@field GetTotalData fun(self: INetChannelInfo, flow: number): number;
+---@field GetTotalPackets fun(self: INetChannelInfo, flow: number): number;
+---@field GetSequenceNr fun(self: INetChannelInfo, flow: number): number;
+---@field IsValidPacket fun(self: INetChannelInfo, flow: number, frame_number: number): boolean;
+---@field GetPacketTime fun(self: INetChannelInfo, flow: number, frame_number: number): number;
+---@field GetPacketBytes fun(self: INetChannelInfo, flow: number, frame_number: number, group: number): number;
+---@field GetStreamProgress fun(self: INetChannelInfo, flow: number, received: ffi.cdata*, total: ffi.cdata*): boolean;
+---@field GetTimeSinceLastReceived fun(self: INetChannelInfo): number;
+---@field GetCommandInterpolationAmount fun(self: INetChannelInfo, flow: number, frame_number: number): number;
+---@field GetPacketResponseLatency fun(self: INetChannelInfo, flow: number, frame_number: number, latency_msecs: ffi.cdata*, choke: ffi.cdata*);
+---@field GetRemoteFramerate fun(self: INetChannelInfo, frame_time: ffi.cdata*, frame_time_std_deviation: ffi.cdata*, frame_start_time_std_deviation: ffi.cdata*);
+---@field GetTimeoutSeconds fun(self: INetChannelInfo): number;
 local INetChannelInfo = memory:class({
     GetName = { 0, 'const char*(__thiscall*)(void*)' },
     GetAddress = { 1, 'const char*(__thiscall*)(void*)' },
@@ -34,91 +62,133 @@ local IEngineClient = memory:interface('engine', 'VEngineClient014', {
     GetNetChannel = { 78, 'void*(__thiscall*)(void*)' },
 });
 
+---@class net_channel_info_t
+---@field this INetChannelInfo
 local net_channel_info_t = {}; do
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_name()
         return ffi.string(self.this:GetName());
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_address()
         return ffi.string(self.this:GetAddress());
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_time()
         return self.this:GetTime();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_time_connected()
         return self.this:GetTimeConnected();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_buffer_size()
         return self.this:GetBufferSize();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_data_rate()
         return self.this:GetDataRate();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:is_loopback()
         return self.this:IsLoopback();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:is_timing_out()
         return self.this:IsTimingOut();
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:is_playback()
         return self.this:IsPlayback();
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_latency(flow)
         return self.this:GetLatency(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_avg_latency(flow)
         return self.this:GetAvgLatency(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_avg_loss(flow)
         return self.this:GetAvgLoss(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_avg_choke(flow)
         return self.this:GetAvgChoke(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_avg_data(flow)
         return self.this:GetAvgData(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_avg_packets(flow)
         return self.this:GetAvgPackets(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_total_data(flow)
         return self.this:GetTotalData(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_total_packets(flow)
         return self.this:GetTotalPackets(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
     function net_channel_info_t:get_sequence_nr(flow)
         return self.this:GetSequenceNr(flow);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@param frame_number number
     function net_channel_info_t:is_valid_packet(flow, frame_number)
         return self.this:IsValidPacket(flow, frame_number);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@param frame_number number
     function net_channel_info_t:get_packet_time(flow, frame_number)
         return self.this:GetPacketTime(flow, frame_number);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@param frame_number number
+    ---@param group number
     function net_channel_info_t:get_packet_bytes(flow, frame_number, group)
         return self.this:GetPacketBytes(flow, frame_number, group);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@return boolean, number, number
     function net_channel_info_t:get_stream_progress(flow)
         local received = ffi.new('int[1]');
         local total = ffi.new('int[1]');
@@ -126,14 +196,22 @@ local net_channel_info_t = {}; do
         return success, received[0], total[0];
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_time_since_last_received()
         return self.this:GetTimeSinceLastReceived();
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@param frame_number number
     function net_channel_info_t:get_command_interpolation_amount(flow, frame_number)
         return self.this:GetCommandInterpolationAmount(flow, frame_number);
     end;
 
+    ---@param self net_channel_info_t
+    ---@param flow number
+    ---@param frame_number number
+    ---@return number, number
     function net_channel_info_t:get_packet_response_latency(flow, frame_number)
         local latency_msecs = ffi.new('int[1]');
         local choke = ffi.new('int[1]');
@@ -141,6 +219,8 @@ local net_channel_info_t = {}; do
         return latency_msecs[0], choke[0];
     end;
 
+    ---@param self net_channel_info_t
+    ---@return number, number, number
     function net_channel_info_t:get_remote_framerate()
         local frame_time = ffi.new('float[1]');
         local frame_time_std_deviation = ffi.new('float[1]');
@@ -149,11 +229,11 @@ local net_channel_info_t = {}; do
         return frame_time[0], frame_time_std_deviation[0], frame_start_time_std_deviation[0];
     end;
 
+    ---@param self net_channel_info_t
     function net_channel_info_t:get_timeout_seconds()
         return self.this:GetTimeoutSeconds();
     end;
 
-    -- Set the metatable for instance method lookup
     net_channel_info_t.__index = net_channel_info_t;
 end;
 
